52948d7a971f55dbdebee2176d9a381c1ff4a3cc
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 22:57:32 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -23,18 +23,6 @@ class Doctor
     Appointment.new(patient, date, self)
   end
 
-  def patients(date = nil)
-    ps = []
-    Appointment.all.each do |appt|
-      if appt.doctor == self
-        ps << appt.patient
-        #binding.pry
-      end
-    end
-
-    ps
-  end
-
   def patients
     Appointment.all.select{|appt| appt.doctor == self}
   end
