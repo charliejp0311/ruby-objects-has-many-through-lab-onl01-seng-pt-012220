08e44a13a9d0f7ac41adb74cbd3f050a32732619
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 21:48:48 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -16,4 +16,18 @@ class Genre
     @@all
   end
 
+  def songs 
+    Song.all.select{|song| song.genre == self}
+  end
+
+  def artists 
+    allStars = []
+    Song.all.each do |song|
+      if song.genre == self
+        allStars << song.artist
+      end
+    end
+    allStars
+  end
+
 end
